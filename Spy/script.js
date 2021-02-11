a6984b76cf37520633af2229b3d2897ecbe8069d
Jitka Svobodová <jitule.92@gmail.com>
2021-02-11 21:01:17 +0100

    Spy created

---
+++ Spy/script.js
@@ -0,0 +1,30 @@
+var firstName = prompt("Enter your first name: ")
+var lastName = prompt("Last name:")
+var age = prompt("Your age:")
+var height = prompt("How tall are you? (cm)")
+var pet = prompt("Your pet name:")
+
+if (firstName[0] == lastName[0]) {
+    var spyName = 1;
+}
+
+if (age > 20 && age < 30) {
+    var spyAge = 1;
+    console.log("age")
+}
+
+if (height >= 170) {
+    var spyHeight = 1;
+    console.log("height")
+}
+
+console.log(pet.length)
+
+if (pet[pet.length - 1] == "y") {
+    var spyPet = 1;
+    console.log("pet")
+}
+
+if (spyName == 1 && spyAge == 1 && spyHeight == 1 && spyPet ==1) {
+    console.log ("I got you spy! This is a secret message :)")
+}
